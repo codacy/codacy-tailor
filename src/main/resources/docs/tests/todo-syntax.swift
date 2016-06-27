75a6a124bae4d8a52c0bf4e837823d89e1e30617
Rafael Cortês <mrfyda@gmail.com>
2016-06-27 09:31:21 +0100

    Pass tests

--- src/main/resources/docs/tests/todo-syntax.swift
+++ src/main/resources/docs/tests/todo-syntax.swift
@@ -1,10 +1,4 @@
-//#Patterns: todo-syntax
 
-//#Issue: {"severity": "Error", "line": 10, "patternId": "todo-syntax"}
-//#Issue: {"severity": "Error", "line": 12, "patternId": "todo-syntax"}
-//#Issue: {"severity": "Error", "line": 14, "patternId": "todo-syntax"}
-//#Issue: {"severity": "Error", "line": 16, "patternId": "todo-syntax"}
-//#Issue: {"severity": "Error", "line": 20, "patternId": "todo-syntax"}
 
 // TODO:
 
