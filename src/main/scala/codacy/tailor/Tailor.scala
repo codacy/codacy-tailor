af8002ffda070793d20416c5cabde01286663f99
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-01-14 13:41:27 +0000

    clean: Refactor code

--- src/main/scala/codacy/tailor/Tailor.scala
+++ src/main/scala/codacy/tailor/Tailor.scala
@@ -8,7 +8,7 @@ import com.codacy.plugins.api.results.Result.{FileError, Issue}
 import com.codacy.plugins.api.results.Tool.Specification
 import com.codacy.plugins.api.results.{Pattern, Result, Tool}
 import com.codacy.plugins.api.{Options, Source}
-import com.codacy.tools.scala.seed.utils.CommandRunner
+import com.codacy.tools.scala.seed.utils.{CommandResult, CommandRunner}
 import com.codacy.tools.scala.seed.utils.ToolHelper._
 import play.api.libs.json._
 
@@ -30,6 +30,77 @@ object Tailor extends Tool {
 
   private lazy val configFileNames = Set(".tailor.yml")
 
+  private def nativeConfigurationFromSource(source: Source.Directory): Option[List[String]] = {
+    configFileNames
+      .map(name => Try(File(source.path) / name))
+      .collectFirst {
+        case Success(file) if file.isRegularFile =>
+          List("-c", file.toJava.getAbsolutePath)
+      }
+  }
+
+  private def patternsToUseFromCodacyConfig(
+      configuration: Option[List[Pattern.Definition]]
+  )(implicit specification: Specification): Option[List[String]] = {
+    val patternsToLintOpt = configuration.withDefaultParameters
+
+    patternsToLintOpt.map {
+      case patternsToLint if patternsToLint.nonEmpty =>
+        val patternIds = patternsToLint.map(_.patternId)
+        val parameters =
+          patternsToLint.flatMap(_.parameters).flatten.flatMap { parameter =>
+            List(s"--${parameter.name}", paramValueToJsValue(parameter.value).toString)
+          }
+        List("--only=" + patternIds.mkString(",")) ++ parameters
+      case _ => List.empty[String]
+    }
+  }
+
+  private def lintingConfiguration(source: Source.Directory, configuration: Option[List[Pattern.Definition]])(
+      implicit specification: Specification
+  ): List[String] = {
+    lazy val nativeConfig = nativeConfigurationFromSource(source)
+
+    patternsToUseFromCodacyConfig(configuration)
+      .orElse(nativeConfig)
+      .getOrElse(List.empty)
+  }
+
+  private def failureMessage(command: List[String], resultFromTool: CommandResult, e: Throwable): String = {
+    s"""
+       |${this.getClass.getSimpleName} exited with code ${resultFromTool.exitCode}
+       |command: ${command.mkString(" ")}
+       |message: ${e.getMessage}
+       |stdout: ${resultFromTool.stdout.mkString(Properties.lineSeparator)}
+       |stderr: ${resultFromTool.stderr.mkString(Properties.lineSeparator)}
+                """.stripMargin
+  }
+
+  def listOfFilesToLint(source: Source.Directory, files: Option[Set[Source.File]]): List[String] = {
+    files.fold(List(source.path)) { paths =>
+      paths.map(_.toString).toList
+    }
+  }
+
+  def commandToRun(patternsConfig: List[String], filesToLint: List[String]): List[String] = {
+    List("/usr/bin/tailor/bin/tailor", "-f", "json") ++ patternsConfig ++ List("--") ++ filesToLint
+  }
+
+  def runCommandOnSourceDir(command: List[String], source: Source.Directory): Try[List[Result]] = {
+    CommandRunner.exec(command) match {
+      case Right(resultFromTool) =>
+        parseToolResult(Paths.get(source.path), resultFromTool.stdout) match {
+          case s @ Success(_) => s
+          case Failure(e) =>
+            val msg = failureMessage(command, resultFromTool, e)
+            Failure(new Exception(msg))
+        }
+
+      case Left(e) =>
+        Failure(e)
+    }
+  }
+
   override def apply(
       source: Source.Directory,
       configuration: Option[List[Pattern.Definition]],
@@ -37,54 +108,13 @@ object Tailor extends Tool {
       options: Map[Options.Key, Options.Value]
   )(implicit specification: Specification): Try[List[Result]] = {
     Try {
+      val filesToLint: List[String] = listOfFilesToLint(source, files)
 
-      lazy val nativeConfig = configFileNames
-        .map(name => Try(File(source.path) / name))
-        .collectFirst {
-          case Success(file) if file.isRegularFile =>
-            List("-c", file.toJava.getAbsolutePath)
-        }
+      val patternsConfig = lintingConfiguration(source, configuration)
 
-      val filesToLint: List[String] = files.fold(List(source.path)) { paths =>
-        paths.map(_.toString).toList
-      }
+      val command = commandToRun(patternsConfig, filesToLint)
 
-      val patternsToLintOpt = configuration.withDefaultParameters
-
-      val conf: Option[List[String]] = patternsToLintOpt.map {
-        case patternsToLint if patternsToLint.nonEmpty =>
-          val patternIds = patternsToLint.map(_.patternId)
-          val parameters =
-            patternsToLint.flatMap(_.parameters).flatten.flatMap { parameter =>
-              List(s"--${parameter.name}", paramValueToJsValue(parameter.value).toString)
-            }
-          List("--only=" + patternIds.mkString(",")) ++ parameters
-        case _ => List.empty[String]
-      }
-
-      val cfgOpt = conf.orElse(nativeConfig).getOrElse(List.empty)
-
-      val command = List("/usr/bin/tailor/bin/tailor", "-f", "json") ++ cfgOpt ++ List("--") ++ filesToLint
-
-      CommandRunner.exec(command) match {
-        case Right(resultFromTool) =>
-          parseToolResult(Paths.get(source.path), resultFromTool.stdout) match {
-            case s @ Success(_) => s
-            case Failure(e) =>
-              val msg =
-                s"""
-                   |${this.getClass.getSimpleName} exited with code ${resultFromTool.exitCode}
-                   |command: ${command.mkString(" ")}
-                   |message: ${e.getMessage}
-                   |stdout: ${resultFromTool.stdout.mkString(Properties.lineSeparator)}
-                   |stderr: ${resultFromTool.stderr.mkString(Properties.lineSeparator)}
-                """.stripMargin
-              Failure(new Exception(msg))
-          }
-
-        case Left(e) =>
-          Failure(e)
-      }
+      runCommandOnSourceDir(command, source)
     }.flatten
   }
 
